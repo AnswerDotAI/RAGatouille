b0078aa3aa8ad53dc2d4be9cc45682add159771a
Benjamin Clavié <ben@clavie.eu>
2024-02-13 21:48:09 +0100

    feat: dynamically assign maximum length (#132)
    * feat: dynamically assign maximum length
    
    * feat: dynamically adjust query size
    
    * fix: typo
    
    * lint
    
    * fix: typo
    
    * fix: query should never sub 32 tokens
    
    * lint

--- ragatouille/models/colbert.py
+++ ragatouille/models/colbert.py
@@ -32,6 +32,7 @@ class ColBERT(LateInteractionModel):
         self.pid_docid_map = None
         self.docid_metadata_map = None
         self.in_memory_docs = []
+        self.base_model_max_tokens = 512
         if n_gpu == -1:
             n_gpu = 1 if torch.cuda.device_count() == 0 else torch.cuda.device_count()
 
@@ -100,6 +101,9 @@ class ColBERT(LateInteractionModel):
         if not training_mode:
             self.inference_ckpt = Checkpoint(
                 self.checkpoint, colbert_config=self.config
+            )
+            self.base_model_max_tokens = (
+                self.inference_ckpt.bert.config.max_position_embeddings
             )
 
         self.run_context = Run().context(self.run_config)
@@ -439,6 +443,14 @@ class ColBERT(LateInteractionModel):
 
         print("Searcher loaded!")
 
+    def _upgrade_searcher_maxlen(self, maxlen: int):
+        if maxlen < 32:
+            # Keep maxlen stable at 32 for short queries for easier visualisation
+            maxlen = 32
+        maxlen = min(maxlen, self.base_model_max_tokens)
+        self.searcher.config.query_maxlen = maxlen
+        self.searcher.checkpoint.query_tokenizer.query_maxlen = maxlen
+
     def search(
         self,
         query: Union[str, list[str]],
@@ -476,8 +488,12 @@ class ColBERT(LateInteractionModel):
         self.searcher.configure(ndocs=max(k * 4, base_ndocs))
 
         if isinstance(query, str):
+            query_length = int(len(query.split(" ")) * 1.35)
+            self._upgrade_searcher_maxlen(query_length)
             results = [self._search(query, k, pids)]
         else:
+            longest_query_length = max([int(len(x.split(" ")) * 1.35) for x in query])
+            self._upgrade_searcher_maxlen(longest_query_length)
             results = self._batch_search(query, k)
 
         to_return = []
@@ -586,17 +602,17 @@ class ColBERT(LateInteractionModel):
             not hasattr(self, "inference_ckpt_len_set")
             or self.inference_ckpt_len_set is False
         ):
-            if max_tokens == "auto" or max_tokens > 512:
-                max_tokens = 512
+            if max_tokens == "auto" or max_tokens > self.base_model_max_tokens:
+                max_tokens = self.base_model_max_tokens
                 percentile_90 = np.percentile(
                     [len(x.split(" ")) for x in documents], 90
                 )
                 max_tokens = min(
                     math.floor((math.ceil((percentile_90 * 1.35) / 32) * 32) * 1.1),
-                    512,
+                    self.base_model_max_tokens,
                 )
                 max_tokens = max(256, max_tokens)
-                if max_tokens > 288:
+                if max_tokens > 300:
                     print(
                         f"Your documents are roughly {percentile_90} tokens long at the 90th percentile!",
                         "This is quite long and might slow down reranking!\n",
@@ -651,6 +667,10 @@ class ColBERT(LateInteractionModel):
     ):
         if isinstance(queries, str):
             queries = [queries]
+        maxlen = max([int(len(x.split(" ")) * 1.35) for x in queries])
+        self.inference_ckpt.query_tokenizer.query_maxlen = max(
+            min(maxlen, self.base_model_max_tokens), 32
+        )
         embedded_queries = [
             x.unsqueeze(0)
             for x in self.inference_ckpt.queryFromText(queries, bsize=bsize)
